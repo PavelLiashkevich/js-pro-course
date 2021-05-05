477eee09abe0f4841b13939748e0c0038b9883dc
Pavel Liashkevich <paul8paul8@mail.ru>
2021-05-05 21:12:39 +0300

    Added homework Lesson 4

--- Lesson 4/script.js
+++ Lesson 4/script.js
@@ -4,6 +4,22 @@
  * метод для инкремента(+1) счетчика, метод для декремента(-1) счетчика, метод который будет возвращать (return) значение счетчика.
  * (использовать this)
  */
+let counter = {
+	count: 0,
+ 
+	inc() {
+		this.count++;
+	},
+
+	decr() {
+	  this.count--;
+	},
+  
+	ret() {
+	  return this.count;
+	},
+  };
+
 
 /**
  * Задание 2
@@ -11,36 +27,33 @@
  * Плюс у него должны быть методы: sayHello - функция, которая принимает имя и
  * будет выводить в alert сообщение 'Привет, ИМЯ! Меня зовут ИМЯ_РАБОТНИКА' (использовать this)
  */
-//let worker = {
-//	firstName: 'John',
-//	lastName: 'Roj',
-//	careerPattern: 7,
-//	email: 'john7roj@gmail.com',
-
-//	sayHello() {
-//		alert(`Hello, ${this.firstName}! My name is ${this.firstName}`);
-//	}
-//}
-//worker.sayHello();
-
-
 /**
  * Задание 3
  * Добавить в объект работника поле количество выполненных деталей. Добавить метод,
  * который будет увеличивать количество деталей на 1. Добавить метод, который будет делать ресет этого количества в 0. (использовать this)
  */
-// let worker = {
-//	firstName: 'John',
-//	lastName: 'Roj',
-//	careerPattern: 7,
-//	email: 'john7roj@gmail.com',
-//	numberDetails: 200,
 
-//	counterDetails() {
+let worker = {
+	firstName: 'John',
+	lastName: 'Roj',
+	careerPattern: 7,
+	email: 'john7roj@gmail.com',
 
-//	}
-//}
-//worker.sayHello();
+	sayHello() {
+		alert(`Hello, Paul! My name is ${this.firstName} ${this.lastName}!`);
+	},
+
+	numberDetails: 200,
+
+	increaseDetails() {
+		this.numberDetails++;
+	},
+
+	resetDetails() {
+		this.numberDetails = 0;
+	}
+}
+worker.sayHello();
 
 
 /**
@@ -51,22 +64,6 @@
  * который будет выводить зарплату работника.
  * Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days.
  */
-function Worker (name, secondName, rate, days, getSalary) {
-	this.name = name;
-	this.secondName = secondName;
-	this.rate = rate;
-	this.days = days;
-
-	this.getSalary = function() {
-		console.log(`${this.rate}` * `${this.days}`);
-	}
-}
-
-let newWorkerOne = new Worker('Paul', 'Smith', 20, 40);
-let newWorkerTwo = new Worker('Steve', 'Jobs', 35, 30);
-
-console.log(newWorkerOne);
-
 /**
  * Задание 5
  * Модифицируйте функцию конструктор Worker из предыдущей задачи следующим образом:
@@ -75,10 +72,76 @@ console.log(newWorkerOne);
  * И также методы setRate, setDays, которые будут устанавливать новые значения для соответственно свойств rate и days.
  */
 
+function Worker (firstName, secondName, rate, days) {
+	this.firstName = firstName;
+	this.secondName = secondName;
+	this.rate = rate;
+	this.days = days;
+
+	this.getSalary = function() {
+		return this.rate * this.days;
+	}
+
+	this.getName = function() {
+		return this.firstName;
+	}
+
+	this.getSecondName = function() {
+		return this.secondName;
+	}
+
+	this.getRate = function() {
+		return this.getRate;
+	}
+
+	this.getDays = function() {
+		return this.getDays;
+	}
+
+	this.setRate = function(newRate) {
+		this.rate = newRate;
+	}
+
+	this.setDays = function(newDays) {
+		this.days = newDays;
+	}
+}
+
+
 /**
  * Задание 6
  * Сделать задание 4-5 с использованием классов.
  */
+class WorkerTwo {
+	constructor(firstName, secondName, rate, days) {
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.rate = rate;
+		this.days = days;
+	}
+	getSalary() {
+		return this.rate * this.days;
+	}
+	getName() {
+		return this.firstName;
+	}
+	getSecondName() {
+		return this.secondName;
+	}
+	getRate() {
+		return this.rate;
+	}
+	getDays() {
+		return this.days;
+	}
+	setRate(newRate) {
+		this.rate = newRate;
+	}
+	setDays(newDays) {
+		this.days = newDays;
+	}
+}
+
 
 /**
  * Задание 7
@@ -90,16 +153,72 @@ console.log(newWorkerOne);
  * который принимает строку и делает заглавной первую букву каждого слова этой строки.
  */
 
+function MyString() {
+	this.reverse = function(str) {
+		return str.split('').reverse().join('');
+	}
+
+	this.ucFirst = function(str) {
+		return str.charAt(0).toUpperCase() + str.slice(1);
+	}
+
+	this.ucWords = function(str) {
+		let splitStr = str.toLowerCase().split(' ');
+		for(let i = 0; i < splitStr.length; i++) {
+			splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+		}
+		return splitStr.join(' ');
+	}
+}
+
+
 /**
  * Задание 8
  * Сделать задание 7 с использованием классов.
  */
+
+class MyStringTwo {
+	reverse = function(str) {
+		return str.split('').reverse().join('');
+	}
+
+	ucFirst = function(str) {
+		return str.charAt(0).toUpperCase() + str.slice(1);
+	}
+
+	ucWords = function(str) {
+		let splitStr = str.toLowerCase().split(' ');
+		for(let i = 0; i < splitStr.length; i++) {
+			splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+		}
+		return splitStr.join(' ');
+	}
+}
+
 
 /**
  * Задание 9
  * Реализуйте класс User, который будет иметь следующие свойства: имя, фамилия, email; следующие методы:
  * getFullName, getEmail.
  */
+
+class User {
+	constructor(firstName, lastName, email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+		getFullName() {
+			return this.firstName + ' ' + this.lastName;
+		}
+
+		getEmail() {
+			return this.email;
+		}
+}
+let user = new User('Pavel', 'Liashkevich', 'paul8paul8@mail.ru')
+console.log(user);
+
 
 /**
  * Задание 10
@@ -110,3 +229,19 @@ console.log(newWorkerOne);
  * который будет выводить текущий курс студента (от 1 до 5). Курс вычисляется так: нужно от текущего года отнять год
  * поступления в вуз. Текущий год получить программно, используя объект Date (https://learn.javascript.ru/datetime).
  */
+
+class Student extends User {
+	constructor(firstName, lastName, email, year) {
+		super(firstName, lastName, email);
+		this.year = year;
+	}
+
+	getFullName() {
+		return this.firstName + '' + this.lastName;
+	}
+
+	getCourse() {
+		const yearNow = new Date().getFullYear();
+		return yearNow - this.year;
+	}
+}
